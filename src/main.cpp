099ac485d12e0eb36abc410193bb9fd7692db885
Anne Brondijk <73667900+abrondijk@users.noreply.github.com>
2022-10-30 13:26:39 +0100

    feat: LVGL implemented

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
-#include "lvgl.h"
-#include "lvgl_helpers.h"
+#include "esp_err.h"
+#include "esp_log.h"
 
-extern "C" void app_main(void)
+#include "display.hpp"
+
+const char *TAG = "MAIN";
+
+extern "C" void app_main()
 {
     printf("Hello world!\n");
 
@@ -15,18 +20,17 @@ extern "C" void app_main(void)
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
     printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
 
     printf("silicon revision %d, ", chip_info.revision);
 
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    
-    lv_init();
+    Display display;
 
-    lvgl_driver_init();
+    display.init(100);
+
 }
-
