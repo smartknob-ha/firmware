b505bd30f61dfb3377be0e69be084732542fd609
Stefan Schokker <cuerda_maijan@simplelogin.com>
2023-01-08 00:21:03 +0100

    \#11 Add very basic  manager test

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,14 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 
+#include "../sdk/manager/include/manager.hpp"
+#include "../sdk/mock_component/include/mock_component.hpp"
+
+void start_smartknob(void) {
+    sdk::mock_component test_component;
+    sdk::manager::instance().add_component(test_component);
+    sdk::manager::instance().start();
+}
 
 extern "C" {
 
@@ -24,13 +32,7 @@ void app_main(void)
     printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
             (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+    start_smartknob();
 }
 
 }
