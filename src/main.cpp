416f630daca275389d6dbbcf2cd246cf29935a95
Stefan Schokker <cuerda_maijan@simplelogin.com>
2023-03-31 22:57:13 +0200

    build: Set specific platform packages for ESP32S3 F_CPU bugfix     - #16     Co-authored-by: Anne Brondijk <abrondijk@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,50 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include <inttypes.h>
+#include "esp_log.h"
 
+#include "../sdk/manager/include/manager.hpp"
+#include "../sdk/mock_component/include/mock_component.hpp"
+//#include "../sdk/led_strip/include/led_strip.hpp"
+
+void start_smartknob(void) {
+    static sdk::mock_component test_component;
+    sdk::manager::instance().add_component(test_component);
+    sdk::manager::instance().start();
+}
 
 extern "C" {
 
-void app_main(void)
-{
-    printf("Hello world!\n");
+void app_main(void) {
+    ESP_LOGI("main", "\nSleeping for 5 seconds before boot...\n");
+    sleep(5);
 
-    /* Print chip information */
+     /* Print chip information */
     esp_chip_info_t chip_info;
+    uint32_t flash_size;
     esp_chip_info(&chip_info);
-    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-            chip_info.cores,
-            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
+    printf("This is %s chip with %d CPU core(s), WiFi%s%s%s, ",
+           CONFIG_IDF_TARGET,
+           chip_info.cores,
+           (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
+           (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
+           (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
 
-    printf("silicon revision %d, ", chip_info.revision);
+    unsigned major_rev = chip_info.revision / 100;
+    unsigned minor_rev = chip_info.revision % 100;
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+    if(esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+        printf("Get flash size failed");
+        return;
+    }
 
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+           (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
 
     for (int i = 10; i >= 0; i--) {
         printf("Restarting in %d seconds...\n", i);
