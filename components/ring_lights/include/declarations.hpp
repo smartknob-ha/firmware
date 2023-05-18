3a6feb40ef524a31628db6dfae1d37d4ff3899bd
Stefan Schokker <cuerda_maijan@simplelogin.com>
2023-05-18 10:26:22 +0200

    Improve ring lights declaration documentation
    See #18

--- components/ring_lights/include/declarations.hpp
+++ components/ring_lights/include/declarations.hpp
@@ -14,36 +14,79 @@ namespace ring_lights {
 
 #define RING_LIGHTS_BYTES_PER_LED 3
 #define RING_LIGHTS_BUFFER_SIZE NUM_LEDS * RING_LIGHTS_BYTES_PER_LED
+
 #define EFFECT_TRANSITION_TICKS 1000 / portTICK_PERIOD_MS
 
 // This way you could set some basic colors, but no patterns
 enum ring_light_effect {
-    // Approximate a point on an angle param_a
-    // Uses primary_color for point color
-    POINTER,
-    // Fill circle starting from param_a until param_a + param_b
-    // Base primary color, gradients into secondary color if provided
-    PERCENT,
-    // Fill entire circle with primary color
-    FILL,
-    // Gradient from primary_color into secondary_color
-    // using param_a as starting angle, param_b as direction angle
-    GRADIENT,
-    // Set every even LED to primary color, every odd LED to secondary color
-    SKIP,
-    // No parameters used, cycles all LED's through a rainbow at the same time
-    RAINBOW_UNIFORM,
-    // Moves a rainbow colour patern around the ring
-    // using param_a as angle rotated per tick
-    RAINBOW_RADIAL,
+	/*
+	* Approximate a point on an angle
+	*
+	* param_a: [int] angle center of point
+	* param_b: [int] angle of width of point
+	*
+	* primary_color: base color, fades at from center
+	*/
+	POINTER,
 
-    EFFECT_MAX  // Leave this at the bottom
+	/*
+	* Fills a bar around the smartknob from start to percentage
+	*
+	* param_a: [int]  starting angle
+	* param_b: [int]  ending angle
+	* param_c: [int]  percentage
+	* param_d: [bool] (counter) clockwise drawing
+	*
+	* primary_color: base color
+	* secondary_color: gradient color if provided (any non-zero)
+	*/
+	PERCENT,
+
+	/*
+	* Fills all LED's with the same color
+	*
+	* primary_color: base color
+	*/
+	FILL,
+
+	/*  -- NOT IMPLEMENTED --
+	* Static gradient
+	*
+	* param_a: [int]  starting angle
+	* param_b: [int]  gradient direction
+	*
+	* primary_color: base color
+	* secondary color: gradient color
+	*/
+	GRADIENT,
+
+	/*	-- NOT IMPLEMENTED --
+	* Set every other led to another color
+	*
+	* primary_color: color for led's on even index
+	* secondary_color: color for led's on odd index
+	*/
+	SKIP,
+
+	/*
+	* Cycles all LED's through a rainbow at the same time
+	*/
+	RAINBOW_UNIFORM,
+
+	/* -- NOT IMPLEMENTED --
+	* Moves a rainbow around the ring
+	*
+	* param_a: [int] rotation per tick
+	*/
+	RAINBOW_RADIAL,
+
+	EFFECT_MAX  // Leave this at the bottom
 };
 
 struct effect_msg {
-    ring_light_effect effect = RAINBOW_UNIFORM;
-    uint16_t param_a = 0, param_b = 0;
-    hsv_t primary_color{0, 0, 0}, secondary_color{0, 0, 0};
+	ring_light_effect effect = RAINBOW_UNIFORM;
+	uint16_t param_a = 0, param_b = 0, param_c = 0, param_d = 0;
+	hsv_t primary_color{0, 0, 0}, secondary_color{0, 0, 0};
 };
 
 typedef void (*effect_func)(rgb_t (&)[NUM_LEDS], effect_msg&);
